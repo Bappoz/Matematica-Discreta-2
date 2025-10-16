a4b09bed8529b5821723fe235e984048451f883a
LUCAS ANDRADE ZANETTI <landradezanetti@gmail.com>
2025-10-15 22:57:48 -0300

    feat: add euclide and pollardRho methods

---
+++ include/pollard.h
@@ -0,0 +1,6 @@
+#ifndef POLLARD_H
+#define POLLARD_H
+
+int pollard(int n);
+
+#endif 
---
+++ src/tools/pollard.c
@@ -0,0 +1,49 @@
+#include "../../include/pollard.h"
+#include "../../include/euclides.h"
+#include <stdio.h>
+#include <math.h>
+
+
+long long int g(int x, int n){
+    return ((x * x) + 1) % n;
+}
+
+
+int pollard(int n){
+    if (n % 2 == 0) return 2;
+
+    long long x = 2; long long int y = 2;
+    int d = 1;
+    int contador = 0;
+    while(d == 1){
+
+        printf("X%d = %lld, Y%d = %lld\n\n", contador, x, contador, y);
+
+        x = g(x, n);
+        printf("G(x) = %lld\n", x);
+        y = g(g(y, n), n);
+        printf("G(y) = %lld\n", y);
+
+        long long int diff = x - y;
+        if(diff < 0) diff = -(diff);
+        printf("|%lld - %lld| = %lld\n\n", x, y, diff);
+        
+        d = mdc(diff, n);
+        printf("\n");
+        contador++;
+    }
+
+    if( d == n ){
+        return -1;
+    }
+
+    return d;
+}
+
+int main(){
+    int n = 8051;
+    int fator = pollard(n);
+    printf("Fator: %d\n", fator);
+
+    return 0;
+}
